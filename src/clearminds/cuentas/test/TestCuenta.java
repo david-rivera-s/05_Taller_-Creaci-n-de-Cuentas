042618605c2ec7d8911e7676a1776181db220917
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-24 15:12:50 -0500

    feat(cuentas): implementar pruebas completas en TestCuenta incluyendo modificacion de valores y cuentas adicionales

--- src/clearminds/cuentas/test/TestCuenta.java
+++ src/clearminds/cuentas/test/TestCuenta.java
@@ -16,5 +16,28 @@ public class TestCuenta {
         cuenta1.imprimir();
         cuenta2.imprimir();
         cuenta3.imprimir();
+        
+     // Modificación de saldos y tipos de las cuentas existentes
+        cuenta1.setSaldo(1000.0);
+        cuenta2.setTipo("A");
+        cuenta2.setSaldo(500.0);
+        cuenta3.setTipo("A");
+        cuenta3.setSaldo(250.0);
+
+        System.out.println("-------- Valores Modificados ---------");
+        cuenta1.imprimir();
+        cuenta2.imprimir();
+        cuenta3.imprimir();
+
+        // Creación de tres cuentas adicionales usando constructores sobrecargados
+        Cuenta cuenta4 = new Cuenta("09876");
+        Cuenta cuenta5 = new Cuenta("11223", "C", 1500.50);
+        Cuenta cuenta6 = new Cuenta("44556");
+        cuenta6.setSaldo(300.0);
+
+        System.out.println("-------- Cuentas Adicionales ---------");
+        cuenta4.imprimir();
+        cuenta5.imprimir();
+        cuenta6.imprimir();
     }
 }
